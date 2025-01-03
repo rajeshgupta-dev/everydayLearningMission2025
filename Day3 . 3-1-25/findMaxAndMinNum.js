ab04618bc2a0f718466aa4f81e1dc4a503baf0f9
rajeshgu <rajeshlkh2020@gmail.com>
2025-01-03 08:39:58 +0530

    Question : Find max and min Without using Build-in Method

---
+++ Day3 . 3-1-25/findMaxAndMinNum.js
@@ -0,0 +1,22 @@
+// Find the max and min number in array
+
+// 1. Without using buid - in method 
+function maxAndMin(arr){
+    
+    let max = arr[0];
+    let min = arr[0];
+    for(let i=1; i<arr.length; i++){
+        if(arr[i] >= max){
+          
+            max = arr[i];
+        }
+        if(arr[i] <= min){
+            min = arr[i];
+       
+        }
+    }
+    console.log("max :",max,"Min :",min);
+}
+maxAndMin([1,2,3,4,5,6,7,8,-9,0,-234,12038]);
+maxAndMin([-1,-2,-34,20,-999,909]);
+maxAndMin([-9087,98554,2345,-49386]);

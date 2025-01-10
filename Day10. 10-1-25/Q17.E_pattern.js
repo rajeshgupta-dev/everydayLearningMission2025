a863e3f5163f73ef7887c8dadd8aa5738b5e36ac
rajeshgu <rajeshlkh2020@gmail.com>
2025-01-10 19:21:40 +0530

    Change in E pattern

--- Day10. 10-1-25/Q17.E_pattern.js
+++ Day10. 10-1-25/Q17.E_pattern.js
@@ -2,7 +2,15 @@
 
 function E_Pattern(n) {
 
-    if (n % 2 !== 0) {
+     if(n <= 0 || n === 1){
+        console.log("Can't Jenerate E Pattern");
+        return ;
+     }
+     if(n % 2 === 0){
+        n = n+1;
+     }
+     
+    if (n >= 2 && n <= 21) {
         let a = Math.ceil(n / 2);   // half of n, for middle line
         for (let i = 1; i <= n; i++) {
             let temp = "";
@@ -30,11 +38,8 @@ function E_Pattern(n) {
             console.log(temp)
         }
     } else {
-        console.log("For making E Pattern ,Please Enter Odd number")
+        console.log(n,"is large Number to Jenerate E pattern, The Number must be b/w 2 and 20.")
     }
-
-
-
 }
 // E_Pattern(3);
 // * * * 
@@ -57,3 +62,21 @@ E_Pattern(7);
 // *
 // *
 // * * * * * * *
+
+E_Pattern(-3);  // Can't Jenerate E Pattern
+E_Pattern(0) ; // Can't Jenerate E Pattern
+E_Pattern(1);  // Can't Jenerate E Pattern
+E_Pattern(22); // 23 is large Number to Jenerate E pattern, The Number must be b/w 2 and 20.
+E_Pattern(10);
+
+// * * * * * * * * * * *
+// *
+// *
+// *
+// *
+// * * * * * * * * * *
+// *
+// *
+// *
+// *
+// * * * * * * * * * * * 

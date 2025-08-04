11f230954f21d82bffdc6bbedbd46f60b78e4cc4
rajeshgu <rajeshlkh2020@gmail.com>
2025-08-04 22:52:07 +0530

    concate 2 str  from starting

--- Day45. 03-8-25/cancateString.js
+++ Day45. 03-8-25/cancateString.js
@@ -1,21 +1,33 @@
 function cancateString(a, b) {
-  let i = a.length - 1;
-  let j = b.length - 1;
-  let c = "";
+  // let i = a.length - 1;
+  // let j = b.length - 1;
+  // let c = "";
 
-  while (i >= 0 || j >= 0) {
-    if (i >= 0) {
-      c += a[i];
-      i--;
-    }
-    if (j >= 0) {
-      c += b[j];
-      j--;
-    }
+  // merze from end 
+  // while (i >= 0 || j >= 0) {
+  //   if (i >= 0) {
+  //     c += a[i];
+  //     i--;
+  //   }
+  //   if (j >= 0) {
+  //     c += b[j];
+  //     j--;
+  //   }
+  // }
+  // console.log(c);
+
+  // merze from start
+  let i = 0;
+  let j = 0;
+  let res = "";
+  while (i < a.length && j < b.length) {
+    res += a[i] + b[j];
+    i++;
+    j++;
   }
-  console.log(c);
+  console.log(res + a.slice(i) + b.slice(j));
 }
-cancateString("rohanRam", "mohan"); // mnaaRhnoamhor
-cancateString("rohan", "mohan"); // nnaahhoorm
-cancateString("Ram", "mohan"); // mnaaRhom
+cancateString("rohanRam", "mohan"); //end -> mnaaRhnoamhor,start -> rmoohhaannRam
+cancateString("rohan", "mohan");    //end -> nnaahhoorm ,  start ->  rmoohhaann
+cancateString("Ram", "mohan");      //end -> mnaaRhom,     start ->  Rmaomhan
 

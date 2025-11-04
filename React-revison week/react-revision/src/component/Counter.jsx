80a8d7d7e546374e90c0cf0c49abb982d6adc6bd
rajeshgu <rajeshlkh2020@gmail.com>
2025-11-04 13:04:47 +0530

    Create Simple counter app

---
+++ React-revison week/react-revision/src/component/Counter.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { useState } from 'react'
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  function increament(){
+    setCount((pre)=>pre+1);
+  }
+  function decreament(){
+   
+    setCount((pre)=>pre-1);
+  }
+  return (
+    <>
+    <div id='container'><h1>{count}</h1>
+      <button onClick={increament} >INC</button>
+      <button onClick={decreament} disabled = {count <= 0}>DEC</button>
+    </div>
+    </>
+  )
+}
+
+export default Counter
